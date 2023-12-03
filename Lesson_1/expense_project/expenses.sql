0b131b6501e9e0dd348b8b6ed727c4f2972bee2c
Nick Perry <nick.perry12@outlook.com>
2023-12-03 14:18:22 -0800

    updating project files

--- Lesson_1/expense_project/expenses.sql
+++ Lesson_1/expense_project/expenses.sql
@@ -4,3 +4,7 @@ CREATE TABLE expenses (
   memo text NOT NULL,
   created_on date NOT NULL DEFAULT CURRENT_DATE
 );
+
+INSERT INTO expenses (amount, memo, created_on) VALUES (14.56, 'Pencils', NOW());
+INSERT INTO expenses (amount, memo, created_on) VALUES (3.29, 'Coffee', NOW());
+INSERT INTO expenses (amount, memo, created_on) VALUES (49.99, 'Text Editor', NOW());
